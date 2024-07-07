06d79b0e15816b9fdb6303c8494f9e8066052aa1
Davi Souza de Oliveira <vlogsplayer10@gmail.com>
2024-07-07 13:46:26 -0300

    fix: fake certificate excluded!

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
-
-// Caminhos para os seus arquivos de certificado
-const keyPath = path.resolve("certificate.key");
-const certPath = path.resolve("certificate.crt");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(certPath),
-    },
   },
 });
